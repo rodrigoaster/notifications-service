9008c1c100e7430fe4ad7b0b1cb643867f76d242
Rodrigo Aster <rodrigowebx@gmail.com>
2023-03-22 20:06:49 -0300

    appController: eslint syntax correction

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,33 +1,29 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './services/prisma.service';
 
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from 'node:crypto';
 import { CreateNotificationBody } from './create-notification-body';
- 
+
 @Controller('notifications')
 export class AppController {
-  constructor(
-    private readonly prsima: PrismaService
-  ) {}
+  constructor(private readonly prsima: PrismaService) {}
 
   @Get()
   list() {
-    return this.prsima.notifications.findMany()
+    return this.prsima.notifications.findMany();
   }
 
   @Post()
-  async create (
-    @Body() body: CreateNotificationBody
-  ) {
+  async create(@Body() body: CreateNotificationBody) {
     const { content, category, recipient_id } = body;
- 
+
     await this.prsima.notifications.create({
       data: {
         id: randomUUID(),
         content,
         category,
         recipient_id,
-      }
-    })
+      },
+    });
   }
 }
