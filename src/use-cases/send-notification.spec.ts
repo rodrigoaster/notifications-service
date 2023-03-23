21d503b47d246fff9275860890884bc61265bbda
Rodrigo Aster <rodrigowebx@gmail.com>
2023-03-22 21:06:35 -0300

    sendNotificationSpec: test using dependency inversion

--- src/use-cases/send-notification.spec.ts
+++ src/use-cases/send-notification.spec.ts
@@ -1,15 +1,21 @@
+import { inMemoryNotificationRepository } from '../../test/repositories/in-memory-notification-repository';
 import { SendNoticiation } from './send-notification';
 
 describe('Send notification', () => {
-  it('should be able send a notification', () => {
-    const sendNotification = new SendNoticiation();
+  it('should be able send a notification', async () => {
+    const notificationsRepository = new inMemoryNotificationRepository();
 
-    const notification = sendNotification.run({
+    const sendNotification = new SendNoticiation(notificationsRepository);
+
+    const { notification } = await sendNotification.run({
       content: 'Nova notificação!',
       category: 'News',
       recipient_id: 'example-id',
     });
 
+    notificationsRepository.create(notification);
+
     expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
